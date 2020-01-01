06325771fa33c5467b983ce71c95aba4187e80b7
twelveapos12 <twelvapos121212@gmail.com>
2020-01-01 23:29:57 +0500

    Добавлена программа для 'автоматической' рисовки текстур

---
+++ AI/__main__.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import pixelate
+import random
+import subprocess
+#!/usr/local/bin/python3
+
+from PIL import Image
+img = Image.open("original/textura.jpg")
+
+rmd = random.randint(1,100)
+
+
+imgSmall = img.resize((16,16),resample=Image.BILINEAR)
+
+
+
+result = imgSmall.resize(img.size,Image.NEAREST)
+name = str(rmd) + ".png"
+
+
+
+result.save(name)
+subprocess.call(['./move.sh'])
